61e32a5ed074459ddf7751ac3607d07c15aa7ec3
Meghan Denny <hello@nektro.net>
2023-09-23 03:03:51 -0700

    collapse spacing in passing cases

--- src/main.zig
+++ src/main.zig
@@ -46,16 +46,16 @@ fn Cases(comptime hex_long: string, comptime asm_long: string) type {
 
 // zig fmt: off
 
-comptime { _ = Cases("8b 44 24 10            ", "mov    eax,DWORD PTR [esp+0x10] "); }
-comptime { _ = Cases("03 44 24 14            ", "add    eax,DWORD PTR [esp+0x14] "); }
-comptime { _ = Cases("c3                     ", "ret                             "); }
-comptime { _ = Cases("b9 25 00 00 00         ", "mov    ecx,0x25                 "); }
-comptime { _ = Cases("ba 10 00 00 00         ", "mov    edx,0x10                 "); }
-comptime { _ = Cases("c7 44 24 04 00 00 00 00", "mov    DWORD PTR [esp+0x4],0x0  "); }
-comptime { _ = Cases("55                     ", "push   ebp"); }
-comptime { _ = Cases("53                     ", "push   ebx"); }
-comptime { _ = Cases("57                     ", "push   edi"); }
-comptime { _ = Cases("56                     ", "push   esi"); }
+comptime { _ = Cases("8b 44 24 10", "mov eax,DWORD PTR [esp+0x10]"); }
+comptime { _ = Cases("03 44 24 14", "add eax,DWORD PTR [esp+0x14]"); }
+comptime { _ = Cases("c3", "ret"); }
+comptime { _ = Cases("b9 25 00 00 00", "mov ecx,0x25"); }
+comptime { _ = Cases("ba 10 00 00 00", "mov edx,0x10"); }
+comptime { _ = Cases("c7 44 24 04 00 00 00 00", "mov DWORD PTR [esp+0x4],0x0"); }
+comptime { _ = Cases("55", "push ebp"); }
+comptime { _ = Cases("53", "push ebx"); }
+comptime { _ = Cases("57", "push edi"); }
+comptime { _ = Cases("56", "push esi"); }
 
 // 00000000 <add>:
 // comptime { _ = Cases("83 ec 0c               ", "sub    esp,0xc                  "); }
